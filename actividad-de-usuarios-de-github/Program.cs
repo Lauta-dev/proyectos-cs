2a88640a1935cce4fd6443f75086daeb13b76c83
lauta-dev <altairdiaz123@gmail.com>
2024-10-14 23:05:51 -0300

    feat: error al no escribir el nombre del usuario

--- actividad-de-usuarios-de-github/Program.cs
+++ actividad-de-usuarios-de-github/Program.cs
@@ -4,18 +4,28 @@ using System.Globalization;
 
 class Program
 {
-    static async Task Main()
+    static async Task Main(string[] args)
     {
-        await GetUsers("lauta-dev");
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Writte a user from GitHub");
+            return;
+        }
+
+        string user = args[0];
+
+        await GetUsers(user);
     }
 
-    public static async Task GetUsers(string username)
+    //TODO: Mejorar el código
+
+    public static async Task GetUsers(string username, int perPage = 5)
     {
         // Parsear fecha
         var cultureInfo = new CultureInfo("es-AR");
-        var format = "dddd 'de' MMMM, HH:mm";
+        var format = "dddd dd 'de' MMMM, HH:mm";
 
-        string url = $"https://api.github.com/users/lauta-dev/events";
+        string url = $"https://api.github.com/users/{username}/events?per_page={perPage}";
         var serializeOptions = new JsonSerializerOptions
         {
             PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
@@ -28,6 +38,12 @@ class Program
             client.DefaultRequestHeaders.Add("User-Agent", "C# App");
             HttpResponseMessage res = await client.GetAsync(url);
 
+            if (!res.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"User {username} no exists");
+                return;
+            }
+
             HttpContent content = res.Content;
             var data = await content.ReadAsStringAsync();
             Github[] gh = JsonSerializer.Deserialize<Github[]>(data, serializeOptions);
@@ -35,15 +51,20 @@ class Program
             for (int i = 0; i < gh.Length; i++)
             {
                 var ghData = gh[i];
-                Console.WriteLine($"Creado el: {Convert.ToDateTime(ghData.CreatedAt, cultureInfo).ToString(format)}");
-                Console.WriteLine($"Tipo: {ghData.Type}");
-                Console.WriteLine($"Repositorio: {ghData.Repo.Name}");
-                Console.WriteLine($"Total de commits: {ghData.Payload.Commits.Length}");
+                Console.WriteLine($"- Creado el: {Convert.ToDateTime(ghData.CreatedAt, cultureInfo).ToString(format)}");
+                Console.WriteLine($"- Tipo: {ghData.Type}");
+                Console.WriteLine($"- Repositorio: {ghData.Repo.Name}");
 
-                foreach (var item in ghData.Payload.Commits)
+                if (ghData.Payload.Commits != null)
                 {
-                    Console.WriteLine($"Mensaje: {item.Message} por {item.Author.Name}");
+                    Console.WriteLine($"- Total de commits: {ghData.Payload.Commits.Length}");
+
+                    foreach (var item in ghData.Payload.Commits)
+                    {
+                        Console.WriteLine($"- Mensaje: {item.Message} por {item.Author.Name}");
+                    }
                 }
+
 
                 Console.WriteLine("---------------------------------");
 
